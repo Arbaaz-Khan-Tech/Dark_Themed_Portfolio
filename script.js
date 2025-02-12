b011d2b8d7062b76b1a9d29a2f5bfa3068f44b98
BaazBoy <arbukhan1971@gmail.com>
2025-02-13 00:49:35 +0530

    Minor Changes

--- script.js
+++ script.js
@@ -25,44 +25,48 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Typewriter effect
-    const typewriter = document.getElementById('typewriter');
-    const roles = [
-        'Cybersecurity Enthusiast',
-        'Ethical Hacker',
-        'Web Developer'
-    ];
-    let roleIndex = 0;
-    let charIndex = 0;
-    let isDeleting = false;
-    let typingSpeed = 100;
-
-    function type() {
-        const currentRole = roles[roleIndex];
-        
-        if (isDeleting) {
-            typewriter.textContent = currentRole.substring(0, charIndex - 1);
-            charIndex--;
-            typingSpeed = 50;
-        } else {
-            typewriter.textContent = currentRole.substring(0, charIndex + 1);
-            charIndex++;
-            typingSpeed = 100;
-        }
-
-        if (!isDeleting && charIndex === currentRole.length) {
-            isDeleting = true;
-            typingSpeed = 1000;
-        } else if (isDeleting && charIndex === 0) {
-            isDeleting = false;
-            roleIndex = (roleIndex + 1) % roles.length;
-            typingSpeed = 500;
-        }
-
-        setTimeout(type, typingSpeed);
-    }
-
-    type();
+ 
 });
+
+    // Typewriter effect
+//     const typewriter = document.getElementById('typewriter');
+//     const roles = [
+//         'Cybersecurity Enthusiast',
+//         'Ethical Hacker',
+//         'Web Developer'
+//     ];
+//     let roleIndex = 0;
+//     let charIndex = 0;
+//     let isDeleting = false;
+//     let typingSpeed = 100;
+
+//     function type() {
+//         const currentRole = roles[roleIndex];
+        
+//         if (isDeleting) {
+//             typewriter.textContent = currentRole.substring(0, charIndex - 1);
+//             charIndex--;
+//             typingSpeed = 50;
+//         } else {
+//             typewriter.textContent = currentRole.substring(0, charIndex + 1);
+//             charIndex++;
+//             typingSpeed = 100;
+//         }
+
+//         if (!isDeleting && charIndex === currentRole.length) {
+//             isDeleting = true;
+//             typingSpeed = 1000;
+//         } else if (isDeleting && charIndex === 0) {
+//             isDeleting = false;
+//             roleIndex = (roleIndex + 1) % roles.length;
+//             typingSpeed = 500;
+//         }
+
+//         setTimeout(type, typingSpeed);
+//     }
+
+//     type();
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
